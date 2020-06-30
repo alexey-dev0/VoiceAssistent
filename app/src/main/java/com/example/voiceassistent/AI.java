eaef70e265096bbcebafeba884ee01c6a8b14586
alexey-dev0 <lesha_43rus@mail.ru>
2020-06-30 23:48:42 +0300

    lab 2 functionality

--- app/src/main/java/com/example/voiceassistent/AI.java
+++ app/src/main/java/com/example/voiceassistent/AI.java
@@ -1,9 +1,16 @@
 package com.example.voiceassistent;
 
 import android.content.Context;
+import android.os.Build;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class AI {
     private static String defaultAnswer;
@@ -20,11 +27,33 @@ public class AI {
     }
 
     public static String getAnswer(String question) {
-        question = question.toLowerCase();
+        question = question.toLowerCase().replace('ё', 'е');
         for (String key : answersDictionary.keySet())
         {
             if (question.contains(key)) return answersDictionary.get(key);
         }
+
+        if (question.contains("сегодня")) {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
+            return "Сегодня " + sdf.format(new Date());
+        } else if (question.contains("час")) {
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", Locale.getDefault());
+            return "Сейчас " + sdf.format(new Date());
+        } else if (question.contains("день недели")) {
+            SimpleDateFormat sdf = new SimpleDateFormat("EEEE", Locale.getDefault());
+            return "Сегодня " + sdf.format(new Date());
+        } else if (question.contains("дней до зачета")) {
+            String dateStr = "2/7/2020";
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                Date date = sdf.parse(dateStr);
+                long diff = date.getTime() - new Date().getTime();
+                return "Дней до зачёта: " + TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
         return defaultAnswer;
     }
 }
--- app/src/main/java/com/example/voiceassistent/MainActivity.java
+++ app/src/main/java/com/example/voiceassistent/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.voiceassistent;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
     protected Button sendButton;
     protected EditText questionText;
     protected TextView chatWindow;
+    protected TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,16 @@ public class MainActivity extends AppCompatActivity {
         });
 
         AI.initialize(this);
+
+        textToSpeech = new TextToSpeech(getApplicationContext(),
+                new TextToSpeech.OnInitListener() {
+                        @Override
+                        public void onInit(int status) {
+                            if (status != TextToSpeech.ERROR) {
+                                textToSpeech.setLanguage(new Locale("ru"));
+                            }
+                        }
+        });
     }
 
     protected void onSend()
@@ -41,5 +56,19 @@ public class MainActivity extends AppCompatActivity {
         String answer = AI.getAnswer(question);
         chatWindow.append(answer);
         chatWindow.append("\n");
+
+        textToSpeech.speak(answer, TextToSpeech.QUEUE_FLUSH, null, null);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putCharSequence("messages", chatWindow.getText());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        chatWindow.setText(savedInstanceState.getCharSequence("messages"));
+        super.onRestoreInstanceState(savedInstanceState);
     }
 }
